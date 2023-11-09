a793ed8cae86a68564937b34019f57a8754f4e4a
Archit <jainarchit1604@gmail.com>
2023-11-10 00:06:43 +0530

    Changed links

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                                 <div class="collapse navbar-collapse" id="navbarNav">
                                     <ul class="navbar-nav">
                                         <li class="nav-item active">
-                                            <a class="nav-link" href="./index.html">Home
+                                            <a class="nav-link" href="./index.php">Home
                                             </a>
                                         </li>
                                         <li class="nav-item">
